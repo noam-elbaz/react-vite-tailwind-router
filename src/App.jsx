0573cbeabaf2e217a9041e92f3b5013e319e1bb6
noam-elbaz <nelbaz@gmail.com>
2023-02-21 15:22:21 +0200

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="bg-red-100 font-bolt text-3xl h-screen w-screen flex justify-center place-items-center">
+      hello
+    </div>
+  );
+}
+
+export default App;
