6279bafbbd296f2c6147605f73679a19db3317dc
noam-elbaz <nelbaz@gmail.com>
2023-02-21 15:49:02 +0200

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="bg-red-100 font-bolt text-3xl h-screen w-screen flex justify-center place-items-center">
-      hello
+    <div className="bg-black h-screen w-screen flex justify-center place-items-center">
+      <div className="flex flex-col text-white">
+        <span className="text-[400px] font-black tracking-tight leading-[300px]">NE</span>
+        <span className="ml-4 uppercase font-extrabold text-[43.5px]">Full-stack Creative</span>
+      </div>
     </div>
   );
 }
